2497d48bafd3195700ffad4ba5b29485bf6a96f9
Arian <arian.kulmer@web.de>
2020-11-28 18:49:15 +0100

    sm6150-common: Do not explicitly include qcom sepolicy
    Change-Id: I9cf2081062f4412f602a42d5e9e225422868e83f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,7 +157,6 @@ VENDOR_SECURITY_PATCH := 2021-03-01
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
