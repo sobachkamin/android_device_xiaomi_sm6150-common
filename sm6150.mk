3c0292ee6788dbd213a2a74d655f4a908a9b387e
Arian <arian.kulmer@web.de>
2020-11-29 01:41:46 +0100

    sm6150-common: Use common bt component
    Change-Id: If813f1ae5916b297210a9363c102d02c660e6b3f

--- sm6150.mk
+++ sm6150.mk
@@ -33,7 +33,6 @@ PRODUCT_PACKAGES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
 # Audio
@@ -91,13 +90,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
@@ -276,6 +268,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
+    bt \
     perf
 
 PRODUCT_COPY_FILES += \
