a1653d46ec5505a20bbe72ae806e50183fd4b6d4
Rahul Shahare <rshaha@codeaurora.org>
2020-12-07 21:31:10 +0530

    sm6150-common: Add kpti=off to boot command line
    This will OFF kernel page table isolation.
    
    Notes from Kuba Wojciechowski <nullbytepl@gmail.com> in https://github.com/tadiphone-caf/msm-4.14/commit/86bb20a8325d32ac678573c86874f36ec4575ee2:
    As per qualcomm - "SM8150/SM8250/SM8350/SM7250/SM7150/SM6150 - KPTI Not required".
    It can also help increase performance by a lot in some scenarios.
    
    Change-Id: Idd36c359d4955701e9ee3ee27a58177a8d622c86
    Signed-off-by: Rahul Shahare <rshaha@codeaurora.org>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -90,6 +90,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0x880000 loop.max_part=7
+BOARD_KERNEL_CMDLINE += kpti=off
 
 BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
