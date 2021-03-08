216c000f68f52ed671fd5a4c745405ae9ae4a1c9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-08 11:43:11 +0530

    sm6150-common: Switch to common QTI display component

--- sm6150.mk
+++ sm6150.mk
@@ -115,22 +115,6 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.4-service \
-    android.hardware.graphics.mapper@3.0-impl-qti-display \
-    android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    vendor.qti.hardware.display.allocator-service
-
-PRODUCT_PACKAGES += \
-    gralloc.sm6150 \
-    hwcomposer.sm6150 \
-    memtrack.sm6150
-
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.display.mapper@2.0.vendor
-
-PRODUCT_PACKAGES += \
     disable_configstore
 
 # DRM
@@ -280,6 +264,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 TARGET_COMMON_QTI_COMPONENTS := \
     adreno \
     bt \
+    display \
     perf \
     telephony
 
