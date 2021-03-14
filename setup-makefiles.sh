6f8d1f1e5cd16e9a35935b7c4a8efd375333fa8e
Arian <arian.kulmer@web.de>
2021-03-14 22:18:04 +0100

    sm6150-common: Initial support for toco
    Change-Id: Ibc8fde452ec3f68971e06526347cce635d5b696a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "**** FILL IN ALL DEVICE NAMES, SPACE DELIMITED ****"
+write_headers "davinci phoenix toco violet"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
